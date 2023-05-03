4fca63a58ec0476f10958a2164715a8884f399d7
medinfo2022 <benfarhat.mohamed9@gmail.com>
2023-05-03 19:19:27 +0100

    First Commit

---
+++ src/app/apixu.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApixuService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(location : any){
+    return this.http.get(
+        
+
+        'http://api.weatherapi.com/v1/current.json?key=e2acb6322ffc4233a7e144824230305&q='+location+'&aqi=no'
+    );
+  }
+}
